fc3e5fdd04afd085174d94d0c53fa33a03dae82a
tuannv <tuannv@quantsville.com>
2022-03-16 14:58:40 +0700

    add total row

--- app/code/Hust/Service/Block/Adminhtml/Report/Grid.php
+++ app/code/Hust/Service/Block/Adminhtml/Report/Grid.php
@@ -5,18 +5,25 @@ namespace Hust\Service\Block\Adminhtml\Report;
 use Magento\Backend\Block\Widget\Grid\Extended;
 use Zend_Date;
 use Hust\Service\Model\ResourceModel\BookingSale\CollectionFactory;
+use Hust\Service\Model\ResourceModel\BookingSale;
 use Magento\Framework\Stdlib\DateTime;
+use Magento\Framework\DataObject;
+use Zend_Db_Expr;
+use Zend_Db_Select;
 
 class Grid extends Extended
 {
     protected $_collectionFactory;
+    protected $resource;
     protected $_columnGroupBy = 'period';
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Backend\Helper\Data $backendHelper,
         CollectionFactory $collection,
+        BookingSale $resource,
         array $data = [])
     {
+        $this->resource = $resource;
         $this->_collectionFactory = $collection;
         parent::__construct($context, $backendHelper, $data);
     }
@@ -38,16 +45,24 @@ class Grid extends Extended
         $collection = $this->_collectionFactory->create();
 
         $params = $this->getFilterData();
-//        if ($params && $params->getData()) {
-//            $from = $params->getFrom();
-//            $to = $params->getTo();
-//            $from = $this->_prepareDateFrom($from);
-//            $to = $this->_prepareDateTo($to);
-//
-//            $collection->addFieldToFilter('date', ['from' => $from . ' 00:00:00', 'to' => $to . ' 23:59:59']);
-//        } else {
-//            $collection->getSelect()->where('main_table.id =?', 0);
-//        }
+        if ($params && $params->getData()) {
+            $from = $params->getFrom();
+            $to = $params->getTo();
+            $from = $this->_prepareDateFrom($from);
+            $to = $this->_prepareDateTo($to);
+            $collection->addFieldToFilter('date', ['from' => $from , 'to' => $to]);
+            if ($locator = $params->getLocatorId()) {
+                $collection->addFieldToFilter('locator_id', ['in' => $locator]);
+            }
+            if ($service = $params->getServiceId()) {
+                $collection->addFieldToFilter('service_id', ['in' => $service]);
+            }
+//            echo $collection->getSelect()->__toString();
+//            die;
+        }
+        else {
+            $collection->getSelect()->where('main_table.id =?', 0);
+        }
 
         $this->setCollection($collection);
         return parent::_prepareCollection();
@@ -58,7 +73,7 @@ class Grid extends Extended
         $start = new Zend_Date($date, DateTime::DATE_INTERNAL_FORMAT);
         $dateStart = new Zend_Date(date("Y-m-d", $start->getTimestamp()), DateTime::DATE_INTERNAL_FORMAT);
         $dateStart->subSecond(3600);
-        return $dateStart->toString('yyyy-MM-dd HH:mm:ss');
+        return $dateStart->toString('yyyy-dd-MM');
     }
 
     protected function _prepareDateTo($date)
@@ -66,7 +81,64 @@ class Grid extends Extended
         $end = new Zend_Date($date, DateTime::DATE_INTERNAL_FORMAT);
         $dateEnd = new Zend_Date(date("Y-m-d", $end->getTimestamp()), DateTime::DATE_INTERNAL_FORMAT);
         $dateEnd->addDay(1)->subSecond(3600)->subMilliSecond(1);
-        return $dateEnd->toString('yyyy-MM-dd HH:mm:ss');
+        return $dateEnd->toString('yyyy-dd-MM');
+    }
+
+    public function getCountTotals()
+    {
+        $read = $this->resource->getConnection();
+        if (!$this->getTotals()) {
+            $collection = $this->_collectionFactory->create();
+
+            $params = $this->getFilterData();
+            if ($params && $params->getData()) {
+                $period = $params->getPeriodType();
+                if ($params->getFrom() && $params->getTo()) {
+                    $collection->addFieldToFilter('date', ['from'=>$params->getFrom() . ' 00:00:00']);
+                    $collection->addFieldToFilter('date', ['to'=>$params->getTo() . ' 23:59:59']);
+                }
+                switch ($period) {
+                    case 'day':
+                        $collection->getSelect()->group('concat(day(date), month(date), year(date))');
+                        break;
+                    case 'month':
+                        $collection->getSelect()->group('concat(month(date), year(date))');
+                        break;
+                    case 'year':
+                        $collection->getSelect()->group('concat(year(date))');
+                        break;
+                }
+
+//                $collection->getSelect()->columns(['charge'=>'sum(*)']);
+                $collection->getSelect()->order('date ASC');
+
+            } else {
+                $collection->getSelect()->where('main_table.id =?', 0);
+            }
+
+            if (count($collection->getItems()) < 1) {
+                $this->setTotals(new \Magento\Framework\DataObject);
+                $this->setCountTotals(false);
+            } else {
+                $ret = $collection->getSelect();
+                $ret->reset(Zend_Db_Select::COLUMNS);
+                $ret->reset(Zend_Db_Select::LIMIT_COUNT);
+                $ret->reset(Zend_Db_Select::LIMIT_OFFSET);
+                $ret->columns([
+                    'charge'  => new Zend_Db_Expr('SUM(charge)'),
+                    'id'=> new Zend_Db_Expr('count(*)'),
+                ]);
+                $data = $read->fetchAssoc($ret);
+                echo $ret->__toString();
+                die;
+                foreach ($data as $item) {
+                    $items = new DataObject();
+                    $items->addData($item);
+                    $this->setTotals($items);
+                }
+            }
+        }
+        return parent::getCountTotals();
     }
 
     /**
@@ -94,16 +166,12 @@ class Grid extends Extended
             'type'  => 'number',
             'sortable'  => false,
             'index' => 'id',
-            'total' => 'sum',
-            'totals_label' => __('Total')
         ]);
 
         $this->addColumn('charge', [
             'header' => __('Charge'),
             'type' => 'number',
             'index' => 'charge',
-            'total' => 'sum',
-            'totals_label' => __('Total')
         ]);
         $this->addExportType('*/*/exportReturnItemsCsv', __('CSV'));
         $this->addExportType('*/*/exportReturnItemsExcel', __('Excel XML'));
