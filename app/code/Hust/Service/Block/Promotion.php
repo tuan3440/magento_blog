b69a1d4706c2d5aca0abc76e368c73b734395d9a
tuannv <tuannv@quantsville.com>
2022-03-25 15:50:53 +0700

    slide promotion

--- app/code/Hust/Service/Block/Promotion.php
+++ app/code/Hust/Service/Block/Promotion.php
@@ -4,20 +4,30 @@ namespace Hust\Service\Block;
 
 use Magento\Framework\View\Element\Template;
 use Hust\Service\Model\ServiceRegistry;
+use Hust\Service\Model\Repository\PromotionRepository;
 
 class Promotion extends Template
 {
     protected $registry;
+    protected $promotionRepo;
     public function __construct(
         ServiceRegistry $registry,
+        PromotionRepository $promotionRepository,
         Template\Context $context, array $data = [])
     {
         $this->registry = $registry;
+        $this->promotionRepo = $promotionRepository;
         parent::__construct($context, $data);
     }
 
     public function getPromotion()
     {
         return $this->registry->registry('current_promotion');
+    }
+
+    public function getAllPromotion()
+    {
+        $promotions = $this->promotionRepo->getListPromotion();
+        return $promotions;
     }
 }
